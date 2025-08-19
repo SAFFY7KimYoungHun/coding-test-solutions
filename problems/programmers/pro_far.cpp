77713ea5ac5f554977c55519a6dc3941ec314d2f
SAFFY7KimYoungHun <104739704+SAFFY7KimYoungHun@users.noreply.github.com>
2025-08-19 21:01:02 +0900

    Add: Programmers far 풀이

---
+++ problems/programmers/pro_far.cpp
@@ -0,0 +1,34 @@
+﻿#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(int n, vector<vector<int>> edge) {
+    int answer = 0;
+    vector<vector<int>> Near(n+1);
+    for(int i=0;i<edge.size();i++){
+        Near[edge[i][0]].push_back(edge[i][1]);
+        Near[edge[i][1]].push_back(edge[i][0]);
+    }
+    vector<int> isVisited(n+1,0);
+    isVisited[1]=1;
+    queue<int> mainQ;
+    mainQ.push(1);
+    while(1){
+        int sizes=mainQ.size();
+        if(!sizes) return answer;
+        answer=sizes;
+        for(int i=0;i<sizes;i++){
+            int past=mainQ.front();
+            mainQ.pop();
+            for(int j=0;j<Near[past].size();j++){
+                if(!isVisited[Near[past][j]]){
+                    isVisited[Near[past][j]]=1;
+                    mainQ.push(Near[past][j]);
+                }
+            }
+        }
+    }
+    return answer;
+}
