75742c9ac8d26fd9cc5e6760da5b9189a8162827
SAFFY7KimYoungHun <104739704+SAFFY7KimYoungHun@users.noreply.github.com>
2025-08-19 19:52:13 +0900

    Refactor: 폴더 구조 정리 (boj/programmers/db)

---
+++ basic/boj/boj_10773.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+#include <stack>
+using namespace std;
+int main() {
+    int K;
+    long long sum=0;
+    stack<int> S;
+    cin>>K;
+    for(int k=0;k<K;k++){
+        int inp;
+        cin>>inp;
+        if(inp){
+            sum+=inp;
+            S.push(inp);
+        }else{
+            sum-=S.top();
+            S.pop();
+        }
+    }
+    return 0;
+}
