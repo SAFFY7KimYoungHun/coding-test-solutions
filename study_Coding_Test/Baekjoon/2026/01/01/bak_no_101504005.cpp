a9eadfd995b9f65f28e9da774f14d01437a030a9
SAFFY7KimYoungHun <104739704+SAFFY7KimYoungHun@users.noreply.github.com>
2026-01-01 17:20:13 +0900

    [2026-01-01][BAK] no_101504005

---
+++ study_Coding_Test/Baekjoon/2026/01/01/bak_no_101504005.cpp
@@ -0,0 +1,22 @@
+﻿#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+
+    int N; cin >> N;
+    priority_queue<pair<int,int>> pq; // (-abs(x), -x)
+
+    while (N--) {
+        int x; cin >> x;
+        if (x != 0) {
+            pq.push({-abs(x), -x});
+        } else {
+            if (pq.empty()) cout << 0 << '\n';
+            else { cout << -pq.top().second << '\n'; pq.pop(); }
+        }
+    }
+}
