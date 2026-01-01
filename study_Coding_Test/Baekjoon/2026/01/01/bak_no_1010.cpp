c94ee6b51cea8886de446c5db68ce492c702b28c
SAFFY7KimYoungHun <104739704+SAFFY7KimYoungHun@users.noreply.github.com>
2026-01-01 18:29:05 +0900

    solve(bak): no_1010

---
+++ study_Coding_Test/Baekjoon/2026/01/01/bak_no_1010.cpp
@@ -0,0 +1,29 @@
+﻿/*
+[문제]
+- 제목: 1010
+- 출처: Baekjoon
+- 난이도: 실버5
+
+[풀이]
+-
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, M;
+        cin >> N >> M;
+
+        long long result = 1;
+        for (int i = 1; i <= N; i++) {
+            result = result * (M - N + i) / i;
+        }
+
+        cout << result << '\n';
+    }
+}
