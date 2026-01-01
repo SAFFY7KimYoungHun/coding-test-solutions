90aa5c8bf74d48578bbe7d32d9ebc48f0dbc08e3
SAFFY7KimYoungHun <104739704+SAFFY7KimYoungHun@users.noreply.github.com>
2026-01-01 18:29:41 +0900

    solve(pro): courses_30_lessons_12929

--- study_Coding_Test/Programmers/2026/01/01/pro_courses_30_lessons_12929.cpp
+++ study_Coding_Test/Programmers/2026/01/01/pro_courses_30_lessons_12929.cpp
@@ -1,9 +1,37 @@
 ﻿/*
 [문제]
-- 제목:
+- 제목: 올바른 괄호의 갯수
 - 출처: Programmers
-- 난이도:
+- 난이도: 4
 
 [풀이]
 -
 */
+#include <string>
+#include <vector>
+#include<queue>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    queue<pair<int,int>>queue_pair;//(개수, )개수
+    queue_pair.push(make_pair(1,0));
+    while(!queue_pair.empty()){
+        pair<int,int> parinow[2]={queue_pair.front(),queue_pair.front()};
+        queue_pair.pop();
+        parinow[0].first+=1;
+        parinow[1].second+=1;
+        if(parinow[0].first<=n){
+            queue_pair.push(parinow[0]);
+        }
+        if(parinow[1].second<=parinow[1].first){
+            if(parinow[1].second==n){
+                answer++;
+                continue;
+            }
+            queue_pair.push(parinow[1]);
+        }
+    }
+    return answer;
+}
